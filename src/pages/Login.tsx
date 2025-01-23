7560773beeecac9aafedee9c46332f9f060f8e3e
Mahin-Hasan <mahinmtrs@gmail.com>
2025-01-24 03:29:08 +0600

    designed basic login and implemented useLoginMutation also set cookies to browser when login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,35 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/api/authApi";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const [login, { data, error, isError, isLoading }] = useLoginMutation();
+  console.log("Data", data);
+  console.log("error", error);
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    login(userInfo);
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+      {/* in antd button type submit does not work */}
+    </form>
   );
 };
 
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/api/v1",
+    credentials: "include", // must need to set credentials or else cookies will not be set in browser
+  }),
   endpoints: () => ({}),
 });
-
