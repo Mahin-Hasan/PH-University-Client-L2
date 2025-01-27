ae412cc9457453f380b8f42500db96e72bbca855
Mahin-Hasan <mahinmtrs@gmail.com>
2025-01-28 03:01:51 +0600

    share form context using FormProvider | React hook form controller for 3rd party input | Resolved type errors | P3

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -1,8 +1,23 @@
-import { useForm } from "react-hook-form";
+import { ReactNode } from "react";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 
-const PHForm = ({ onSubmit, children }) => {
-  const { handleSubmit } = useForm();
-  return <form onSubmit={handleSubmit(onSubmit)}>{children}</form>;
+type TFormProps = {
+  onSubmit: SubmitHandler<FieldValues>;
+  children: ReactNode;
+};
+
+const PHForm = ({ onSubmit, children }: TFormProps) => {
+  const methods = useForm();
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
 };
 
 export default PHForm;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
-import { Button } from "antd";
-import { FieldValues, useForm } from "react-hook-form";
+import { Button, Row } from "antd";
+import { FieldValues } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser, TUser } from "../redux/features/auth/authSlice";
@@ -7,53 +7,52 @@ import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import PHForm from "../components/form/PHForm";
+import PHInput from "../components/form/PHInput";
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { register, handleSubmit } = useForm();
-
-  const [login, { error }] = useLoginMutation();
+  // const { register, handleSubmit } = useForm();
+  const [login] = useLoginMutation();
   // console.log("error", error);
   const onSubmit = async (data: FieldValues) => {
     console.log(data);
 
-    // const toastId = toast.loading("Logging in");
-    // try {
-    //   const userInfo = {
-    //     id: data.id,
-    //     password: data.password,
-    //   };
-    //   const res = await login(userInfo).unwrap();
-    //   const user = verifyToken(res.data.accessToken) as TUser;
-    //   dispatch(setUser({ user, token: res.data.accessToken }));
-    //   // console.log(res);
-    //   toast.success("User Logged in Successfully✅", {
-    //     id: toastId,
-    //     duration: 2000,
-    //   });
-    //   navigate(`/${user.role}/dashboard`);
-    // } catch (err) {
-    //   toast.error(`something went wrong!!`, {
-    //     id: toastId,
-    //     duration: 2000,
-    //   });
-    // }
+    const toastId = toast.loading("Logging in");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      dispatch(setUser({ user, token: res.data.accessToken }));
+      // console.log(res);
+      toast.success("User Logged in Successfully✅", {
+        id: toastId,
+        duration: 2000,
+      });
+      navigate(`/${user.role}/dashboard`);
+    } catch (err) {
+      toast.error(`something went wrong!!`, {
+        id: toastId,
+        duration: 2000,
+      });
+    }
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="id">ID:</label>
-        <input type="text" id="id" {...register("id")} />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="text" id="password" {...register("password")} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-      {/* in antd button type submit does not work */}
-    </PHForm>
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <PHForm onSubmit={onSubmit}>
+        {/* <label htmlFor="id">ID:</label> */}
+        {/* <input type="text" id="id" {...register("id")} /> */}
+        <PHInput label="ID:" type="text" name="id" />
+        {/* <label htmlFor="password">Password:</label> */}
+        <PHInput label="Password:" type="password" name="password" />
+        <Button htmlType="submit">Login</Button>
+        {/* in antd button type submit does not work */}
+      </PHForm>
+    </Row>
   );
 };
 
