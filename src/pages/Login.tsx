5387a586eb3f3ac75d70b34e7a1dacbd852b31f3
Mahin-Hasan <mahinmtrs@gmail.com>
2025-01-28 01:34:02 +0600

    resolve TS error in baseApi and make a form wrapper

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,7 @@ import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import PHForm from "../components/form/PHForm";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -15,31 +16,33 @@ const Login = () => {
   const [login, { error }] = useLoginMutation();
   // console.log("error", error);
   const onSubmit = async (data: FieldValues) => {
-    const toastId = toast.loading("Logging in");
-    try {
-      const userInfo = {
-        id: data.id,
-        password: data.password,
-      };
-      const res = await login(userInfo).unwrap();
-      const user = verifyToken(res.data.accessToken) as TUser;
-      dispatch(setUser({ user, token: res.data.accessToken }));
-      // console.log(res);
-      toast.success("User Logged in Successfully✅", {
-        id: toastId,
-        duration: 2000,
-      });
-      navigate(`/${user.role}/dashboard`);
-    } catch (err) {
-      toast.error(`something went wrong!!`, {
-        id: toastId,
-        duration: 2000,
-      });
-    }
+    console.log(data);
+
+    // const toastId = toast.loading("Logging in");
+    // try {
+    //   const userInfo = {
+    //     id: data.id,
+    //     password: data.password,
+    //   };
+    //   const res = await login(userInfo).unwrap();
+    //   const user = verifyToken(res.data.accessToken) as TUser;
+    //   dispatch(setUser({ user, token: res.data.accessToken }));
+    //   // console.log(res);
+    //   toast.success("User Logged in Successfully✅", {
+    //     id: toastId,
+    //     duration: 2000,
+    //   });
+    //   navigate(`/${user.role}/dashboard`);
+    // } catch (err) {
+    //   toast.error(`something went wrong!!`, {
+    //     id: toastId,
+    //     duration: 2000,
+    //   });
+    // }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <PHForm onSubmit={onSubmit}>
       <div>
         <label htmlFor="id">ID:</label>
         <input type="text" id="id" {...register("id")} />
@@ -50,7 +53,7 @@ const Login = () => {
       </div>
       <Button htmlType="submit">Login</Button>
       {/* in antd button type submit does not work */}
-    </form>
+    </PHForm>
   );
 };
 
