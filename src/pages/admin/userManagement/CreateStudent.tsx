ef6358f56ebd6bd0b4091b50a10a4ea589b5b1cf
Mahin-Hasan <mahinmtrs@gmail.com>
2025-02-05 04:04:42 +0600

    Create student basic form structure

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Col, Divider, Row } from "antd";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
@@ -13,10 +13,11 @@ const studentDummyData = {
     },
     gender: "male",
     dateOfBirth: "1990-01-01",
+    bloogGroup: "A+",
+    
     email: "student2@gmail.com",
     contactNo: "1235678",
     emergencyContactNo: "987-654-3210",
-    bloogGroup: "A+",
     presentAddress: "123 Main St, Cityville",
     permanentAddress: "456 Oak St, Townsville",
     guardian: {
@@ -42,17 +43,43 @@ const CreateStudent = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
 
-    const formData = new FormData(); //* must use new as FormData is a constructor func
+    // const formData = new FormData(); //* must use new as FormData is a constructor func
 
-    formData.append("data", JSON.stringify(data));
+    // formData.append("data", JSON.stringify(data));
     // console.log(Object.fromEntries(formData)); //? Understanding how form data works
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" label="Name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Row>
+      <Col span={24}>
+        <PHForm onSubmit={onSubmit}>
+          <Divider>Personal Info</Divider>
+          <Row gutter={8}>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              {/* antd uses 24 col so 8 x 3 = 24 */}
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.middleName" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.lastName" label="Last Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              {/* antd uses 24 col so 8 x 3 = 24 */}
+              <PHInput type="text" name="gender" label="Gender" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="dateOfBirth" label="Date of Birth" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="bloogGroup" label="Blood Group" />
+            </Col>
+          </Row>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
   );
 };
 
